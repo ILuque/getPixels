3c7c0a02e96303597c56594df66a80c5925571d2
ILuque <i>
2016-04-14 19:07:31 +0200

    canvas resizes to image size

--- js/main.js
+++ js/main.js
@@ -8,35 +8,59 @@ console.info('updated');
 
 
     pub.updateWrap = function() {
-        var d = dataWrapForm(); //check form
-        draw(d.imageId, d.nodesX, d.nodesY, d.text); //draw new image
+        var f = dataWrapForm(); //check form
+          console.info(f.canvasX, f.canvasY);
+        
+        draw(f.imageId, f.nodesX, f.nodesY, f.text); //draw new image
     };
 
     // gets data from wrapper Form
     function dataWrapForm() {
-        var form = {};
-        form.nodesX = +document.getElementById('nodesX').value;
-        form.nodesY = +document.getElementById('nodesY').value;
+        var f = {};
+        f.nodesX = +document.getElementById('nodesX').value;
+        f.nodesY = +document.getElementById('nodesY').value;
 
-        var wrapper = document.getElementById('wrapper');
+        var wrapper = document.getElementById('nodeWrapper');
         wrapper.style.width = document.getElementById('wrapperX').value + "px";
         wrapper.style.height = document.getElementById('wrapperY').value + "px";
 
-        form.text = document.getElementById('text').value;
-        form.radios = document.getElementsByClassName('imgRadio');
+        f.text = document.getElementById('text').value;
+        f.radios = document.getElementsByClassName('imgRadio');
 
-        for (var i = 0; i < form.radios.length; i++) {
-            if (form.radios[i].type === 'radio' && form.radios[i].checked) {
+        for (var i = 0; i < f.radios.length; i++) {
+            if (f.radios[i].type === 'radio' && f.radios[i].checked) {
                 // get value, set checked flag or do whatever you need to
-                form.image = form.radios[i].value;
+                f.image = f.radios[i].value;
             }
         }
-        console.log(form);
-        return { imageId: form.image, nodesX: form.nodesX, nodesY: form.nodesY, text: form.text };
+
+        //canvas
+        f.canvasX = +document.getElementById('canvasX').value;
+        f.canvasY = +document.getElementById('canvasY').value;
+
+        console.log(f);
+        console.log(f.canvasX);
+        return { imageId: f.image, nodesX: f.nodesX, nodesY: f.nodesY, text: f.text, canvasX: f.canvasX, canvasY: f.canvasY };
     }
 
 
-    //Canvas in variables
+
+
+    // create canvas
+    function renewCanvas(x, y) {
+        var wrapperCv = document.getElementById("canvasWp");
+        var oldCv = document.getElementById("myCanvas");
+
+        // new canvas
+        var canvas = document.createElement('canvas');
+        canvas.id = "myCanvas";
+        canvas.width = x;
+        canvas.height = y;
+
+        wrapperCv.removeChild(oldCv);
+        wrapperCv.appendChild(canvas);
+
+    }
 
 
     /* draws the image using nodes
@@ -47,9 +71,18 @@ console.info('updated');
         if (jumpX < 1 || isNaN(jumpX)) jumpX = 1;
         if (jumpY < 1 || isNaN(jumpY)) jumpY = 1;
 
-        var canvas = document.getElementById('myCanvas');
-        var wrapper = document.getElementById('wrapper');
+        // image to use
         var img = document.getElementById(imageId);
+
+        // canvas of the same size of image
+        renewCanvas(img.width, img.height);
+
+        // canvas (origin) and wrapper (HTML node wrapper)
+        var canvas = document.getElementById('myCanvas');
+        var nodeWrap = document.getElementById('nodeWwrapper');
+        
+
+
 
         // draw img in canvas (getPixelColor gets info from it)
         var ctx = canvas.getContext("2d");
@@ -75,14 +108,14 @@ console.info('updated');
             for (x = 0; x < canvasWidth; x += jumpX, nodeX++) {
                 structure += "<li id='li:" + nodeX + "." + nodeY +
                     "'' style='background:rgba(" +
-                    getPixelColor(canvas, x , y ) +
+                    getPixelColor(canvas, x, y) +
                     ");'>" + inNode + "</li>";
             }
             structure += " </ul>";
         }
         structure += "</ul>";
 
-        wrapper.innerHTML = structure;
+        nodeWrap.innerHTML = structure;
     } // \draw function
 
     function getPixelColor(canvas, x, y) {
